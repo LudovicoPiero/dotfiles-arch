c47561a0dfa21b5a6b520801358813ba324fb04c
Ludovico <ludovicopiero@pm.me>
2022-10-14 18:51:21 +1000

    Updateeeee

--- fish/config.fish
+++ fish/config.fish
@@ -9,5 +9,15 @@ alias gcm='cz c'
 alias gdl='gallery-dl --config ~/.config/gallery-dl/gallery-dl.conf'
 alias hx='helix'
 alias sudo='doas'
+alias delcache='sudo paccache -rk3; yay -Sc --aur --noconfirm'
+alias update='export TMPFILE="$(mktemp)"; \
+    sudo true; \
+    rate-mirrors --save=$TMPFILE arch --max-delay=21600 \
+      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
+      && delcache \
+      && yay -Syyu --noconfirm'
+
+
 starship init fish | source
 fish_add_path /home/ludovico/.spicetify
