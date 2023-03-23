014fc9e604cd4422ae1f2678b2a982b999f498f0
Norina Madoka <ludovicopiero@pm.me>
2023-03-24 06:33:01 +1000

    add fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,34 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias g='git'
-    alias gpl='git pull'
-    alias gpsh='git push -u origin'
-    alias gcm='cz c'
     alias gdl='gallery-dl --config ~/.config/gallery-dl/gallery-dl.conf'
     alias sudo='doas'
+    alias n='nvim'
     alias nv='nvim'
+
+    # Color stuff
+    alias ls='exa --icons'
+    alias l='exa -lbF --git --icons'
+    alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons'
+    alias ll='exa -lbGF --git --icons'
+    alias llm='exa -lbGF --git --sort=modified --icons'
+    alias cat='bat --theme gruvbox-dark'
+    alias grep='grep --color=auto'
+    alias tree='exa --tree --icons'
+    alias '..'='cd ..'
+    alias '...'='cd ../..'
+
+    # Git Aliases
+    alias g='git'
+    alias ga='git add'
+    alias gaa='git add .'
+    alias gm='git commit'
+
+    alias gf='git fetch'
+    alias gs='git status'
+    alias gpl='git pull'
+    alias gpsh='git push -u origin'
 
     # Path and stuff
     fish_add_path -m ~/.bin/
     fish_add_path -m ~/.local/bin/
-
-    starship init fish | source
 end
---
+++ .config/fish/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias g='git'
+    alias gpl='git pull'
+    alias gpsh='git push -u origin'
+    alias gcm='cz c'
+    alias gdl='gallery-dl --config ~/.config/gallery-dl/gallery-dl.conf'
+    alias sudo='doas'
+    alias nv='nvim'
+
+    # Path and stuff
+    fish_add_path -m ~/.bin/
+
+    starship init fish | source
+end
