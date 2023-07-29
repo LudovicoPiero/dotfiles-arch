7d08a2202b1fe3c7c7cf373f3099cce806a50758
Ludovico Piero <ludovicopiero@pm.me>
2023-07-29 11:31:21 +1000

    Yay

--- nvchad/init.lua
+++
@@ -1,6 +0,0 @@
--- tells GitHub Copilot to assume that all the commands are already mapped
-vim.g.copilot_assume_mapped = true
-
--- Set to relative line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
